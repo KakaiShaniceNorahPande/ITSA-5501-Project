deb6068e479b8fb20ff2d6179f3c71a8e3d15134
KakaiShaniceNorahPande <pandenorah878@gmail.com>
2026-04-08 14:31:48 -0400

    updated files

---
+++ iac/main.tf
@@ -0,0 +1,102 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "canadacentral"
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVMVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "myVMSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "publicip" {
+  name                = "myVMPublicIP"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "myVMNic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.publicip.id
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "myLinuxVM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("C:/Users/pande/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  disable_password_authentication = true
+
+  tags = {
+    Environment = "Dev"
+  }
+}
